e734e5628fad0371780476bb2fe4ccd4861b7763
sneha Jain <snehajain08178@gmail.com>
2020-05-09 17:57:05 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+class App extends Component {
+  render(){
+    return (
+      <ScrollView>
+        <Text>jdkj</Text>
+      </ScrollView>
+    )
+  }
+}
+
+
+
+export default App;
